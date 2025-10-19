5006d8dd459b08357d02c89ed933786a1eaf04bf
leuvarishabh <leuvarishabh@gmail.com>
2025-10-19 19:47:31 +0530

    update check

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,5 +1,21 @@
 package dao;
 
-public class UserDao {
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-}
+public class UserDao {
+	
+	public void savecustomer() {
+		Transaction tx = null;
+		
+		try {
+			Object util;
+
+					
+			
+		} catch (Exception e) {
+			System.out.println("data not inserted");
+		}
+		
+
+}}
